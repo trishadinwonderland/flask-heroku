538c22ce88c44cd12d4fe6d8403b91041bc1ae99
trishadd <89003949+trishadinwonderland@users.noreply.github.com>
2021-09-06 13:44:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,9 +29,7 @@ def hello():
 def get_api():
     return jsonify(data)
 
-@app.route('/hello/<string:name>')
-def hello01():
-    return "สวัสดี"    
+   
 
 if __name__ == "__main__":
     app.run(debug=False)
